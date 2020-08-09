07c9eb00665efc8d320222c1f435894c5395c6d5
Rahul Kalithkar <rahulkalithkar1996@gmail.com>
2020-08-09 14:58:12 +0530

    modified installation file

--- installations.sh
+++ installations.sh
@@ -3,4 +3,4 @@ sudo apt-get update -y
 sudo apt-get install docker.io -y
 sudo usermod -aG docker ubuntu
 sudo service docker start
-sudo docker run -p 8585:8085 -d kalithkar/casestudy_spring_app:2.0
+sudo docker run -d -p 8585:8085 kalithkar/casestudy_spring_app:2.0
